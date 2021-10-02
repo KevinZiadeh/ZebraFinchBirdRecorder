39b062bcb0712537c871763c1d42ab23952db36e
Kevin Ziadeh <kevinziadeh@gmail.com>
2021-10-02 16:04:49 +0300

    Added filter comparision with MATLAB

--- C++/test.cpp
+++ C++/test.cpp
@@ -8,47 +8,52 @@
 
 using namespace std;
 
+#define print_vec(vec) for(auto e: vec) {cout << e << " ";} cout << endl;
 #define BUFFER_SIZE 4096
 
-float* test_filter(float* raw_signal, long size);
-void compare_matlab(vector<float> vectored_signal,float* filtered_signal, int size);
+double* test_filter(double* raw_signal, long size);
+void compare_matlab(vector<double> vectored_signal,double* filtered_signal, int size);
 
 int main(){
-    float* audioBuffer1 = (float *)malloc(sizeof(float)*BUFFER_SIZE);
-    float* audioBuffer2 = (float *)malloc(sizeof(float)*BUFFER_SIZE);
+    double* audioBuffer1 = (double *)malloc(sizeof(double)*BUFFER_SIZE);
+    double* audioBuffer2 = (double *)malloc(sizeof(double)*BUFFER_SIZE);
 
     int buffer1_head = 0;
     int buffer2_head = 0;
     int buffer1_tail = BUFFER_SIZE;
     int buffer2_tail = BUFFER_SIZE;
 
-    vector<float> data = read_csv("data.csv");
-    float* raw_signal = (float *)malloc(sizeof(float)*data.size());
+    vector<double> data = read_csv("data.csv", 1);
+    double* raw_signal = (double *)malloc(sizeof(double)*data.size());
     for (long i =0; i<data.size();i++){
         raw_signal[i] = data[i];
     }
-    long size = data.size();
+    int size = data.size();
 
-
-    float* filtered_signal = test_filter(raw_signal, size);
+    double* filtered_signal = test_filter(raw_signal, size);
     this_thread::sleep_for(1s);
     compare_matlab(data, filtered_signal, size);
 
     return 0;
 }
 
-float* test_filter(float* raw_signal, long size){
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    float* filtered_signal = filter_signal(raw_signal, size);
-    std::chrono::steady_clock::time_point end1 = std::chrono::steady_clock::now();
-    std::cout << "Filtering function took " << std::chrono::duration_cast<std::chrono::microseconds>(end1 - begin).count() << "muS" << std::endl;
+double* test_filter(double* raw_signal, long size){
+    this_thread::sleep_for(1s);
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    double* filtered_signal = filter_signal(raw_signal, size);
+    chrono::steady_clock::time_point end1 = chrono::steady_clock::now();
+    cout << "Filtering function took " << chrono::duration_cast<chrono::microseconds>(end1 - begin).count() << "muS" << endl;
     return filtered_signal;
 }
 
-void compare_matlab(vector<float> vectored_signal,float* filtered_signal, int size){
+void compare_matlab(vector<double> vectored_signal, double* filtered_signal, int size){
 
+    vector<double> matlab_filtered_signal = read_csv("matlab_firfiltered.csv", 0);
+    double precision = 0.0000000001;
     for (int i =0;i<size;i++){
-        cout << "Volt: " << vectored_signal[i] << " - FIR: " << filtered_signal[i] << endl;
+        cout << "Volt: " << vectored_signal[i] << " - C++ FIR: " << filtered_signal[i] 
+        << " - MATLAB FIR: " << matlab_filtered_signal[i] << " - Equal? " 
+        << (abs(filtered_signal[i]-matlab_filtered_signal[i]<precision) ? "True" : "False") << endl;
     }
 
 }
